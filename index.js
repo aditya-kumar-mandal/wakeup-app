b6d5816322901f4d1de5cbca888f5af16dbd9cac
akm-akm <adityanick08@gmail.com>
2021-06-30 00:42:19 +0530

    heroku r10 error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = new express();
-const port = 3000;
+const port = process.env.PORT|| 3000;
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
