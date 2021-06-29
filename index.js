ed5cdef6a60c497ac16404e93c149a8414d8bfe5
akm-akm <adityanick08@gmail.com>
2021-06-29 18:12:36 +0530

    good

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const server = new express();
+const port = 3000;
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+server.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+console.clear();
+sitelist = JSON.parse(fs.readFileSync("./database.json"));
+server.listen(port, () => {
+    console.log("\nrunnning on http://localhost:3000\n");
+    
+});
+
+server.get("/", (req, res) => {
+    console.log("/");
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+server.post("/submit", (req, res) => {
+    console.log(req.body);
+    if (sitelist.site.includes(req.body.appname)) {
+        console.log("already present");
+        res.send("already present");
+        return;
+    } else {
+        fs.readFile("./database.json", (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                obj = JSON.parse(data);
+                obj.site.push(req.body.appname);
+                fs.writeFile(
+                    "./database.json",
+                    JSON.stringify(obj, null, "\t"),
+                    "utf8",
+                    () => {}
+                );
+            }
+        });
+        res.send("added");
+    }
+});
+
+setInterval(() => {
+    sitelist.site.forEach((element) => {
+        axios
+            .get("https://" + element + ".herokuapp.com")
+            .then(() => {
+                console.log("success - " + element);
+            })
+            .catch(() => {
+                console.log("error - " + element);
+            });
+    });
+}, 1500000);
