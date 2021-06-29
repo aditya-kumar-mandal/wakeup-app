44e65a7d012b8ce74d3340a34a49f978d986f25b
akm-akm <adityanick08@gmail.com>
2021-06-30 00:34:26 +0530

    ok

--- index.js
+++ index.js
@@ -4,53 +4,69 @@ const port = 3000;
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
+
 server.use(
     express.urlencoded({
         extended: true,
     })
 );
 console.clear();
-sitelist = JSON.parse(fs.readFileSync("./database.json"));
+
 server.listen(port, () => {
     console.log("\nrunnning on http://localhost:3000\n");
     
 });
+
 server.get("/", (req, res) => {
  
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 server.get("/akm123", (req, res) => {
-    res.send(sitelist.site);
+    fs.readFile("./database.json", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data);
+           res.send(obj)
+        }
+    })
 });
 
 server.post("/submit", (req, res) => {
-    console.log(req.body);
-    if (sitelist.site.includes(req.body.appname)) {
-        console.log("already present");
-        res.send("already present");
-        return;
-    } else {
-        fs.readFile("./database.json", (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                obj = JSON.parse(data);
-                obj.site.push(req.body.appname);
+    y=req.body.appname.trim();
+    console.log(y);
+
+    fs.readFile("./database.json", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data);
+            if (obj.site.includes(y)) {
+                console.log("already present");
+                res.sendFile(path.join(__dirname, "/error.html")); 
+            } else{
+                obj.site.push(y);
                 fs.writeFile(
                     "./database.json",
                     JSON.stringify(obj, null, "\t"),
                     "utf8",
                     () => {}
                 );
+                res.sendFile(path.join(__dirname, "/ok.html"));
             }
-        });
-        res.send("added");
-        console.log("added");
-    }
-});
+        }
+    })
+
+})
 
 setInterval(() => {
+
+
+    try {
+      
+    sitelist = JSON.parse(fs.readFileSync("./database.json"));
+    console.log("----------------------- " );
     sitelist.site.forEach((element) => {
         axios
             .get("https://" + element + ".herokuapp.com")
@@ -58,7 +74,12 @@ setInterval(() => {
                 console.log("success - " + element);
             })
             .catch(() => {
-                console.log("error - " + element);
+                console.log("error   - " + element);
             });
-    });
-}, 1500000);
+    });  
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+}, 5000);
